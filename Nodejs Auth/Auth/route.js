a218452d0a3b77d10da812645dbbf70811709784
TAFAT Abdelouahab <48151472+abdelouahab-tafat@users.noreply.github.com>
2022-04-28 14:27:31 +0200

    final auth

--- Nodejs Auth/Auth/route.js
+++ Nodejs Auth/Auth/route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, update, deleteUser } = require("./auth");
+
+const { register, login, update, deleteUser, getUsers } = require("./auth");
+const { adminAuth } = require("../middleware/auth");
+
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/update").post(update);
-router.route("/deleteUser").delete(deleteUser);
+router.route("/update").put(adminAuth, update);
+router.route("/deleteUser").delete(adminAuth, deleteUser);
+router.route("/getUsers").get(getUsers);
+
 module.exports = router;
