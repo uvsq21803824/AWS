3a643b0aaa513b8059a38ee4e83736e10d5cfee9
timingattack <98774703+timingattack@users.noreply.github.com>
2022-05-14 21:32:29 +0200

    Update server.js

--- Authentification Nodejs/server.js
+++ Authentification Nodejs/server.js
@@ -7,61 +7,62 @@ const { adminAuth, userAuth } = require("./middleware/auth.js");
 const PORT = 5000;
 
 //////////////////////////////ANTI FAILLES DE SECURITE/////////////////////////////////////////////////////////////////////////////////////
-var  casque  =  require ( 'casque' ) ;
-var  xss  =  require ( 'x-xss-protection' ) ;
-var  xFrameOptions  =  require ( 'x-frame-options' ) ;
-var  redirectToHTTPS  =  require ( 'express-http-to-https' ) . redirigerVersHTTPS ;
+var helmet = require('helmet');
+var xss = require('x-xss-protection');
+var xFrameOptions = require('x-frame-options');
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
-//sécurité de contenu
-application . utiliser ( casque . contentSecurityPolicy ( {
-  directive : {
-    frameAncestors : [ "'self'" ] ,
+//sécurit de contenu
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    frameAncestors:["'self'"],
   }
-} ) ) ;
+}));
 
-// empêcher le vol de session
-const  session  =  require ( 'cookie-session' )
-var  dateexpiration  =  nouvelle  date ( Date . maintenant ( )  +  60  *  60  *  1000 )  // 1 heure
-application . utiliser ( session ( {
-    nom : 'toto' ,
-    clés : [ 'clé1' ,  'clé2' ] ,
-    biscuit : {
-        sécurisé : vrai ,
-        httpOnly : vrai ,
-        chemin : 'foo/bar' ,
-        expire : dateexpiration
+//empêcher le vol de session
+const session = require('cookie-session')
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+app.use(session({
+    name: 'toto',
+    keys: ['key1', 'key2'],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        path: 'foo/bar',
+        expires: expiryDate
     }
-} ) ) ;
+}));
 
-// forcer le navigateur à communiquer en https de manière privilégiée avec le serveur
-var  soixanteJoursEnSecondes = 5184000  ; 
-application . use ( casque . hsts ( {  maxAge : sixtyDaysInSeconds  } ) ) ;
-//partie envoyant l'utilisateur dans la zone sécurisée
-var  express_enforces_ssl  =  require ( 'express-enforces-ssl' ) ;
-application . utiliser ( express_enforces_ssl ( ) ) ;
+// forcer le navigateur à communiquer en https de manière privilégié avec le serveur
+var sixtyDaysInSeconds = 5184000;
+app.use(helmet.hsts({ maxAge: sixtyDaysInSeconds }));
+//partie envoyant l’utilisateur dans la zone sécurisée
+var express_enforces_ssl = require('express-enforces-ssl');
+app.use(express_enforces_ssl());
 
 //masquer l'en-tête Referer
-application . use ( casque . referrerPolicy ( {  policy : 'same-origin'  } ) ) ; //
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));//
 //empêche les navigateurs d'essayer de deviner ("sniff") le type MIME
-application . utilisation ( casque . noSniff ( ) ) ;
+app.use(helmet.noSniff());
 //supprimer l'en-tête X-Powered-By
-application . utiliser ( casque . hidePoweredBy ( ) ) ;
+app.use(helmet.hidePoweredBy());
 //anticickjajing
-application . use ( casque . frameguard ( {  action : 'sameorigin'  } ) )
+app.use(helmet.frameguard({ action: 'sameorigin' }))
 
-/*deny: on interdit tout simplement au navigateur d'inclure cette page dans une iframe;
-  sameorigin : on autorise seulement pour une page venant du même endroit que la page de l'iframe ;
-  allow - from : on spécifique qui peut l'afficher.Pratique notamment si vous avez une application agrégeant plusieurs autres applications.*/
+/*deny: on interdit tout simplement au navigateur d’inclure cette page dans une iframe;
+  sameorigin: on autorise seulement pour une page venant du même endroit que la page de l’iframe;
+  allow - from : on spécifie qui peut l’afficher.Pratique notamment si vous avez une application agrégeant plusieurs autres applications.*/
 
 
 //failles xss
-application . utiliser ( casque . xssFilter ( ) )
-application . use ( xss ( {  setOnOldIE : true  } ) ) ;
+app.use(helmet.xssFilter())
+app.use(xss({ setOnOldIE: true }));
 
 //injection sql
-var  app  =  express ( ) ;
-var  sqlinjection  =  require ( 'sql-injection' ) ;
-application . use ( sqlinjection ) ;
+var app = express();
+var sqlinjection = require('sql-injection');
+app.use(sqlinjection);
+//########################################################################################################################################//
 //############################################### ################################################# ######################################//
 
 
