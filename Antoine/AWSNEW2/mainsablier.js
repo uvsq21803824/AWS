f8817fb05ef56fa28f091906c89107ee5222f68e
uvsq21803824 <cambresy.florian@gmail.com>
2022-05-22 20:44:40 +0200

    ajout du timeur qui fonctionne 1min par defaut

--- Antoine/AWSNEW2/mainsablier.js
+++ Antoine/AWSNEW2/mainsablier.js
@@ -128,11 +128,28 @@ document.addEventListener("DOMContentLoaded", () => {
 		selected = false;
 		level++;
 		alea = Math.floor(Math.random() * question.length);
-		
-		if (level==15) {
-			finish();
-			return;
-		}
+
+		if (level==1){
+            const startingMinutes = 1;
+            let time = startingMinutes * 60;
+
+            const countdownEl = document.getElementById('countdown');
+
+            setInterval(updateCountdown,1000);
+
+            function updateCountdown() {
+                const minutes = Math.floor(time/60);
+                let seconds = time % 60;
+
+                countdownEl.innerHTML = `${minutes}: ${seconds}`;
+
+                let number = time--;
+                if (minutes == 0 && seconds == 0){
+                    finish();
+                    return;}
+
+            }
+        }
 
 		addButtons(true);
 		updateProgress();
